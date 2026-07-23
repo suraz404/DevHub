4f8a0fdd271a3c933b2d61ad1d0569990e8db3af
Suraz Agrahari <surazagrahari1980@gmail.com>
2026-07-24 00:01:23 +0545

    fix: correct spacing in Hero component for improved layout

--- features/hero/Hero.tsx
+++ features/hero/Hero.tsx
@@ -9,9 +9,9 @@ import HeroSearch from "./HeroSearch";
 
 const Hero = () => {
   return (
-    <Section className="  relative  flex items-center overflow-hidden">
+    <Section className=" relative  flex items-center overflow-hidden">
       <Container>
-        <div className="mx-auto  flex max-w-5xl flex-col items-center text-center">
+        <div className="mx-auto mt-16 flex max-w-5xl flex-col items-center text-center">
           <HeroBadge />
 
           <HeroHeading />
